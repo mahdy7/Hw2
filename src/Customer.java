26315a67c1f31208c86d18e8bc4d65daeebe54a0
Dean <deanamir01@gamil.com>
2025-05-18 23:16:51 +0300

    I added three functions to the customer, each takes care of the renting, unrenting, and showing the Customer and his movies that he rented.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,44 @@
+import java.sql.SQLOutput;
+
 public class Customer {
+    private String customerName;
+    private String customerId;
+    private Movie [] currentMovies;
+    private int currentMovieIndex;
+
+    public Customer(String customerName,String customerId,Movie [] currentMovies) {
+        this.currentMovies = new Movie[5];
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.currentMovieIndex = 0;
+    }
+
+    public void show(){
+        System.out.println("Name"+ customerName +",ID"+ customerId) ;
+        for(int i = 0; i < this.currentMovieIndex; i++){
+            System.out.println(this.currentMovies[i]);
+        }
+    }
+
+    public boolean addMovie(Movie movie){
+        if(this.currentMovieIndex < 5){
+            this.currentMovies[this.currentMovieIndex] = movie;
+            this.currentMovieIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean returnMovie(Movie movie){
+        for(int i = 0; i < this.currentMovieIndex; i++){
+            if(this.currentMovies[i].equals(movie)){
+                this.currentMovies[i] = this.currentMovies[this.currentMovieIndex - 1];
+                //replacing the movie we removed with the last one;
+                this.currentMovies[this.currentMovieIndex-1] = null;
+                this.currentMovieIndex--;
+                return true;
+            }
+        }
+        return false;// if the movie wasn't found.
+    }
 }
