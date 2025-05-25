897ea9ca784a1aaf4886360880565fc67b38d690
mimem <mimemmaru10@gmail.com>
2025-05-25 18:11:26 +0300

    documentation for Director,Movie

--- src/Director.java
+++ src/Director.java
@@ -10,7 +10,15 @@ public class Director {
         return this.directorName;
     }
 
-    // implementation change??
+    /**
+     * finds a director object from a given array using only the director's name
+     * returns the index of the director in the array
+     * in case the director isn't found returns -1
+     *
+     * @param directorName the wanted director's name
+     * @param directors the array of directors
+     * @return directors index (-1 if not found)
+     */
     public static int findDirector(String directorName,Director[] directors){
         int counter = 0;
         if (directors == null) {return -1;}
@@ -26,15 +34,22 @@ public class Director {
         return this.directorName.equals(director.getDirectorName());
     }
 
+    /**
+     * if the director of the movie that will be removed directed only that movie
+     * he will be removed from the directors array
+     *
+     * @param movieCounter how many movies are currently in the system
+     * @param movies the array of movies currently in the system
+     * @param movieIndex the index of the movie that will be removed
+     * @param directors the array of directors
+     */
     public static void removeDirector(int movieCounter,Movie [] movies,int movieIndex, Director[] directors){
         int noOtherDirector = 0;
-        //removing the director
         for(int i = 0;i < movieCounter;i++) {
             if(movies[movieIndex].movieDirector().equals(movies[i].movieDirector())){
                 noOtherDirector++;
             }
         }
-
         if(noOtherDirector == 1){
             int directorIndex= Director.findDirector(movies[movieIndex].movieDirector().getDirectorName(),directors);
             directors[directorIndex] = directors[directorIndex-1];
--- src/Movie.java
+++ src/Movie.java
@@ -5,6 +5,13 @@ public class Movie {
     private Director director;
     private boolean rented;
 
+    /**
+     * constructs the movie object
+     * @param title the name of the movie.
+     * @param genre its genre.
+     * @param year the year it was released in.
+     * @param director the person who directed it.
+     */
     Movie(String title,Genre genre,int year,Director director){
         this.title = title;
         this.genre = genre;
@@ -13,28 +20,40 @@ public class Movie {
         this.rented = false;
     }
 
-    public String movieTitle(){
-        return this.title;
-    }
+    public String movieTitle(){ return this.title; }
 
-    public Director movieDirector(){
-        return this.director;
-    }
+    public Director movieDirector(){ return this.director; }
 
-    public int movieYear(){
-        return this.year;
-    }
-
-    public void rent(boolean rented){this.rented = rented;}
+    public int movieYear(){ return this.year; }
 
     public boolean isRented() {return this.rented;}
 
+    /**
+     * changes the state of the movie (rented or not rented)
+     * @param rented the new state of the movie (rented or not rented)
+     */
+    public void rent(boolean rented){ this.rented = rented;}
+
+    /**
+     * equal function for movie objects
+     *
+     * @param movie the movie object
+     * @return true if this.movie and the given movie are the same
+     */
     private boolean equals(Movie movie) {
+        if (movie == null) return false;
         return this.title.equals(movie.movieTitle()) &&
                 this.director.equals(movie.movieDirector()) &&
                 this.year == movie.movieYear();
     }
-    //change name
+
+    /**
+     * searches an array of movies for this.movie
+     *
+     * @param movies the array of movies.
+     * @param movieCounter how many movies are in the movies array.
+     * @return returns true if this.movies is in the movies array.
+     */
     public boolean findMovie(Movie[] movies,int movieCounter){
         for (int i = 0; i < movieCounter; i++ ){
             if(this.equals(movies[i])){return true;}
@@ -42,6 +61,17 @@ public class Movie {
         return false;
     }
 
+    /**
+     * finds a movie object in the movies array using only the movie's name ,its release year and its directors name
+     *
+     *
+     * @param movieName the name of the movie
+     * @param year the year the movie was released
+     * @param director the name of the director
+     * @param movies the array of movies
+     * @param movieCounter how many movies are in the array
+     * @return movie's index (-1 if not found)
+     */
     public static int findMovie(String movieName,int year,String director,Movie[] movies,int movieCounter){
         for (int i = 0; i < movieCounter; i++ ){
             if (movieName.equals(movies[i].movieTitle()) &&
@@ -52,6 +82,10 @@ public class Movie {
         return -1;
     }
 
+    /**
+     * prints this.movie's properties in the following manner
+     *  "Title: <>, Genre: <>, Year: <>, director: <> "
+     */
     public void printMovie(){
         System.out.println("Title: " + this.title + ", Genre: " + this.genre + ", Year: "
                 + this.year + ", director: " + this.director.getDirectorName());
